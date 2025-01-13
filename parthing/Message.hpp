2cd681f681c9877a8b9dee90acb5830091af2302
Denos-soneD <81745153+Denos-soneD@users.noreply.github.com>
2025-01-13 16:58:34 +0100

    feat: add client connection handling

---
+++ parthing/Message.hpp
@@ -0,0 +1,34 @@
+#ifndef MESSAGE_HPP
+#define MESSAGE_HPP
+
+#include <string>
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+#define MAX_MESSAGE_SIZE 512
+
+class Message
+{
+private:
+	std::string _raw_message;
+	std::string _prefix;
+	std::string _command;
+	std::vector<std::string> _parameters;
+
+	void parse();
+	bool isValidMessage() const;
+
+public:
+	explicit Message(const std::string &message);
+
+	const std::string &getPrefix() const;
+	const std::string &getCommand() const;
+	const std::vector<std::string> &getParameters() const;
+	const std::string &getRawMessage() const;
+};
+
+std::ostream &operator<<(std::ostream &os, const Message &msg);
+
+#endif
