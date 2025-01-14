cf1cdd593af4b82502d6cad42e29ef8379f3d972
supe4cookie <supe4cookie@gmail.com>
2025-01-14 23:37:05 +0100

    feat: add connection management

--- code/server/Channel.cpp
+++ code/server/Channel.cpp
@@ -1,9 +1,17 @@
 #include <Channel.hpp>
 
-Channel::Channel(/* args */)
+Channel::Channel(std::string name, Client *owner) : name(name), owner(owner)
 {
 }
 
 Channel::~Channel()
 {
+}
+
+void Channel::broadcast(std::string message)
+{
+	for (std::vector<Client>::iterator it = clients.begin(); it != clients.end(); ++it)
+	{
+		it->sendReply("PRIVMSG", message);
+	}
 }
