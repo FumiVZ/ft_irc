c51ac173a1eb15cec0e9606bcd73ab92ae81d720
Denos-soneD <81745153+Denos-soneD@users.noreply.github.com>
2025-02-27 14:46:07 +0100

    fix: patch bot yellow letter display

--- code/bot/Wordle.cpp
+++ code/bot/Wordle.cpp
@@ -50,26 +50,57 @@ std::string Wordle::randomword()
 std::string Wordle::playWordle(std::string &word)
 {
 	std::string result = "";
+	if (word.substr(0, word.find('\r')) == this->Word)
+	{
+		result = "YOU WIN!";
+		this->Word = randomword();
+		return result;
+	}
 	for (int i = 0; i < 5; i++)
 	{
 		if (word[i] == this->Word[i])
 		{
 			result += "\033[32m" + std::string(1, word[i]) + "\033[0m";
 		}
-		else if (this->Word.find(word[i]) != std::string::npos)
-		{
-			result += "\033[33m" + std::string(1, word[i]) + "\033[0m";
-		}
 		else
 		{
-			result += word[i];
+			int targetCount = 0;
+			for (size_t k = 0; k < this->Word.length(); k++)
+			{
+				if (this->Word[k] == word[i])
+					targetCount++;
+			}
+
+			int usedCount = 0;
+			for (int j = 0; j < i; j++)
+			{
+				if (word[j] == word[i] &&
+					(word[j] == this->Word[j] ||
+					 this->Word.find(word[j]) != std::string::npos))
+				{
+					usedCount++;
+				}
+			}
+
+			bool letterFound = false;
+			for (size_t m = 0; m < this->Word.length(); m++)
+			{
+				if (this->Word[m] == word[i])
+				{
+					letterFound = true;
+					break;
+				}
+			}
+
+			if (letterFound && usedCount < targetCount)
+			{
+				result += "\033[33m" + std::string(1, word[i]) + "\033[0m";
+			}
+			else
+			{
+				result += word[i];
+			}
 		}
-	}
-	if (word.substr(0, word.find('\r')) == this->Word){
-		result = "YOU WIN!";
-		this->Word = randomword();
 	}
 	return result;
 }
-
-
